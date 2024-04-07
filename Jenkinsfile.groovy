6f7715978f1e963168842115fc598391fbae177f
Richard <164130786@qq.com>
2024-04-07 17:51:26 +0800

    submit to github actions - add jenkinsfile to support jenkins in offline env

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage("build") {
+            steps {
+                echo "building"
+            }
+        }
+        stage("test") {
+            steps {
+                sh './tools/ci/test_controller.sh core'
+            }
+        }
+    }
+}
